7cc98a28448ed1167030c771069dd6756b1a7d0e
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2026-02-20 02:55:00 -0500

    Add LUKS device configuration for encrypted data

--- configuration.nix
+++ configuration.nix
@@ -30,6 +30,11 @@
   '';
 };
 
+boot.initrd.luks.devices."crypted_data" = {
+  device = "/dev/disk/by-id/ata-ST4000VN006-3CW104_ZW6365MF-part1";
+  keyFile = "/persist/etc/secrets/data_drive.key";
+};
+
 
   # --- Initrd SSH (The Fort Knox Early-Boot Unlock) ---
   #boot.initrd.network = {
