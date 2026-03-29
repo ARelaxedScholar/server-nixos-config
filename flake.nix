d8e64442a331d56f40d391fc1c197d39524a3ca1
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2026-03-28 23:07:05 -0400

    Update animus URL in flake.nix

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     disko.url = "github:nix-community/disko/latest";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     impermanence.url = "github:nix-community/impermanence";
-    animus.url = "path:/home/user/server-nixos-config/services/animus.nix";  
+    animus.url = "path:/home/user/Animus";  
     animus.inputs.nixpkgs.follows = "nixpkgs";
   };
 
