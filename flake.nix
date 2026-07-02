64a0d17f8f8109bb64f55fd180c59527e1d8b98b
Ange Kouakou <ange@swagwatch.app>
2026-07-02 02:40:37 +0000

    chore: stop ignoring flake files

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "Multi-host NixOS configuration for swagwatch-engine and thesentry";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    disko.url = "github:nix-community/disko/latest";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+    impermanence.url = "github:nix-community/impermanence";
+    animus.url = "github:ARelaxedScholar/Animus";
+    animus.inputs.nixpkgs.follows = "nixpkgs";
+    llm-agents.url = "git+https://github.com/numtide/llm-agents.nix?rev=53673313e86582f3ac7050ff826158fd843c219d";
+    swagwatch-engine.url = "git+file:///mnt/data/swagwatch-engine";
+    swagwatch-engine.inputs.nixpkgs.follows = "nixpkgs";
+    swagwatch-engine.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Forge is developed locally on this host for now.
+    forge.url = "path:/home/user/workspace/forge";
+    forge.inputs.nixpkgs.follows = "nixpkgs";
+
+    watchtower.url = "git+ssh://git@gitlab.com/swagwatch/observability/watchtower.git";
+    uriel = {
+      url = "git+ssh://git@gitlab.com/arelaxedscholar-group/uriel.git";
+      flake = true;
+    };
+  };
+
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      openshell-pkg = pkgs.callPackage ./packages/openshell { };
+
+      mkHost =
+        {
+          modules,
+          extraSpecialArgs ? { },
+        }:
+        nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {
+            inherit inputs openshell-pkg;
+          }
+          // extraSpecialArgs;
+          inherit modules;
+        };
+    in
+    {
+      packages.${system} = {
+        openshell = openshell-pkg.openshell-cli;
+        openshell-gateway = openshell-pkg.openshell-gateway;
+      };
+
+      nixosConfigurations = {
+        swagwatch-engine = mkHost {
+          extraSpecialArgs = {
+            animus = inputs.animus;
+            llm-agents = inputs.llm-agents;
+            swagwatch-engine = inputs.swagwatch-engine;
+            forge = inputs.forge;
+            watchtower = inputs.watchtower;
+            uriel = inputs.uriel;
+            inherit openshell-pkg;
+          };
+          modules = [
+            ./modules/common/base.nix
+            ./hosts/swagwatch-engine/default.nix
+            ./modules/common/openshell.nix
+            inputs.disko.nixosModules.disko
+            inputs.impermanence.nixosModules.impermanence
+          ];
+        };
+
+        thesentry = mkHost {
+          extraSpecialArgs = { llm-agents = inputs.llm-agents; };
+          modules = [
+            ./modules/common/base.nix
+            ./hosts/thesentry/default.nix
+            inputs.disko.nixosModules.disko
+          ];
+        };
+      };
+    };
+}
