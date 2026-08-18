14b334d4b7fce8f19c90f05a75be5fe5cfb39ece
Ange Kouakou <ange@swagwatch.app>
2026-08-18 22:57:52 +0000

    deploy Uriel provider resilience hotfix

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-1a51e552";
+      url = "path:/mnt/data/vendor/uriel-737a2217";
       flake = true;
     };
   };
