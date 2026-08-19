c308798c4bde8dfb57ab84aeae3ee9133833977a
Ange Kouakou <ange@swagwatch.app>
2026-08-19 17:54:05 +0000

    Deploy Uriel safe web retrieval

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-f53b357";
+      url = "path:/mnt/data/vendor/uriel-bce9dc8";
       flake = true;
     };
   };
