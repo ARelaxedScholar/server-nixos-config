68b57fa9a1d95702f8cc68655d3ea5cd881ca1f2
Ange Kouakou <ange@swagwatch.app>
2026-08-19 23:14:23 +0000

    Deploy verified Uriel commitment handoff

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-6a093c0";
+      url = "path:/mnt/data/vendor/uriel-fa2c6c8";
       flake = true;
     };
   };
