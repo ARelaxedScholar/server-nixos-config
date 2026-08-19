ce6cb65e2c8e87514b98221e6c9a644abcf09854
Ange Kouakou <ange@swagwatch.app>
2026-08-19 19:46:27 +0000

    Deploy Uriel stored test validation

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-d7277c7";
+      url = "path:/mnt/data/vendor/uriel-e852233";
       flake = true;
     };
   };
