401d05656425ae6bdd34720c75b4445bab55698f
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2026-08-19 16:49:02 +0000

    Deploy Uriel successor disposition guard

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-70525d13";
+      url = "path:/mnt/data/vendor/uriel-99758b35";
       flake = true;
     };
   };
