a3b80c5c7f4c497632caaa3deebe513a27970dbf
Ange Kouakou <ange@swagwatch.app>
2026-08-19 22:45:02 +0000

    Deploy bounded Uriel recovery actions

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-8c22454";
+      url = "path:/mnt/data/vendor/uriel-6fc8601";
       flake = true;
     };
   };
