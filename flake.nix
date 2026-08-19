12960d4d15b487586492134a2c096819cf5003a2
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2026-08-19 16:19:31 +0000

    Deploy Uriel synthesis tool authority

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-308b4b73";
+      url = "path:/mnt/data/vendor/uriel-2042cdc7";
       flake = true;
     };
   };
