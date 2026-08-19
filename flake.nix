9e1c1bdc7a37a4953310da3e5d26dcf1fad9f059
Ange Kouakou <ange@swagwatch.app>
2026-08-19 23:04:49 +0000

    Deploy deterministic Uriel follow-through

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-e99a635";
+      url = "path:/mnt/data/vendor/uriel-6a093c0";
       flake = true;
     };
   };
