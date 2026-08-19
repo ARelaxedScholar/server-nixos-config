0317e5dec817e33daec075b2bd75995d0b08d44a
Ange Kouakou <ange@swagwatch.app>
2026-08-19 22:55:45 +0000

    Deploy Uriel code artifact test transition

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-6fc8601";
+      url = "path:/mnt/data/vendor/uriel-e99a635";
       flake = true;
     };
   };
