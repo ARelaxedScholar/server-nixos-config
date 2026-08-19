5bbd6f3f45682ad413db8133044b279aa2a6805e
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2026-08-19 11:51:25 +0000

    Deploy grounded autonomous Uriel loop

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-11cf1995";
+      url = "path:/mnt/data/vendor/uriel-90ecbd0c";
       flake = true;
     };
   };
