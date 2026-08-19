900b9e69bd1678b474c7fe2ca22e7ec82ebd28b1
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2026-08-19 12:20:53 +0000

    Guarantee Uriel autonomous discovery fallback

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-9f5d40ee";
+      url = "path:/mnt/data/vendor/uriel-e9d1890e";
       flake = true;
     };
   };
