2ca45e3d2a1da834dca45b2d68273f75d9af859c
Ange Kouakou <ange@swagwatch.app>
2026-08-19 17:27:51 +0000

    Deploy Uriel stalled-search synthesis fix

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-ef303107";
+      url = "path:/mnt/data/vendor/uriel-07b93e9";
       flake = true;
     };
   };
