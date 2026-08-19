768c1c6a5ed6d5b177de484225a9996e5bfe3373
Ange Kouakou <ange@swagwatch.app>
2026-08-19 20:06:24 +0000

    Deploy Uriel semantic failure repair

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-2addc86";
+      url = "path:/mnt/data/vendor/uriel-3c7fe57";
       flake = true;
     };
   };
