2ebc2081e82326f911a43fe74c268c45835e06b6
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2026-08-19 15:30:44 +0000

    Deploy Uriel foreground commitment loop

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-7bcc7a29";
+      url = "path:/mnt/data/vendor/uriel-e8c927bb";
       flake = true;
     };
   };
