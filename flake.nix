2c72fd67c715ae28c2095b9377929cd5d940daa7
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2026-08-19 13:25:22 +0000

    Preserve Uriel autonomous next actions

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-666d4f53";
+      url = "path:/mnt/data/vendor/uriel-7bcc7a29";
       flake = true;
     };
   };
