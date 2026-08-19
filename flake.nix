9b5ce5e0967f49e32be8c19abd6149edbc845ca3
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2026-08-19 13:17:11 +0000

    Compact Uriel research continuation evidence

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-5c93f70d";
+      url = "path:/mnt/data/vendor/uriel-666d4f53";
       flake = true;
     };
   };
