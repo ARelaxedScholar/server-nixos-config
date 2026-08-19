9bc28ea8cee5dc792be258dea4ce255fbcd473b6
Ange Kouakou <ange@swagwatch.app>
2026-08-19 18:10:53 +0000

    Deploy Uriel documentation fetch capacity

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-a5a4495";
+      url = "path:/mnt/data/vendor/uriel-169ab81";
       flake = true;
     };
   };
