5001a8b99ce8e590681589bc42cea8c3a01b3bd4
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2026-08-19 12:02:40 +0000

    Harden Uriel autonomous execution

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-90ecbd0c";
+      url = "path:/mnt/data/vendor/uriel-3d9ce243";
       flake = true;
     };
   };
