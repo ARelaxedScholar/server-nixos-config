812c0da26706152efa5bfc4ccd3736205922b2b7
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2026-08-19 12:10:47 +0000

    Reject Uriel autonomous filesystem busywork

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-3d9ce243";
+      url = "path:/mnt/data/vendor/uriel-9f5d40ee";
       flake = true;
     };
   };
