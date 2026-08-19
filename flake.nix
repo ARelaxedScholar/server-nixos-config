d8f13d30f5dd6a000ea0364e8ea1a64eeb87e130
Kouakou Jedidiah Ange-Emmanuel <angemmanuel.kouakou+professional@gmail.com>
2026-08-19 16:28:42 +0000

    Deploy Uriel artifact transition

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-2042cdc7";
+      url = "path:/mnt/data/vendor/uriel-488595b1";
       flake = true;
     };
   };
