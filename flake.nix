58235a4d8d679bdb6f45ca2d4b2105062901c1cb
Ange Kouakou <ange@swagwatch.app>
2026-08-20 14:52:36 +0000

    Deploy multiline Uriel task completions

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-271cc05";
+      url = "path:/mnt/data/vendor/uriel-51d2077";
       flake = true;
     };
   };
