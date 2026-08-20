f56ac04c25929d222cc7e45100489e8399238892
Ange Kouakou <ange@swagwatch.app>
2026-08-20 19:55:56 +0000

    deploy strict shell result validation

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-d60937d";
+      url = "path:/mnt/data/vendor/uriel-80fad6b";
       flake = true;
     };
   };
