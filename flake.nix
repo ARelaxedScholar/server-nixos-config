7e0e656c75854352f290fb6f0aff2604a3b27a76
Ange Kouakou <ange@swagwatch.app>
2026-08-20 20:05:39 +0000

    deploy blocker authority enforcement

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-80fad6b";
+      url = "path:/mnt/data/vendor/uriel-899pending";
       flake = true;
     };
   };
