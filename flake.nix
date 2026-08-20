ad26c3403abc1fde8722878377170d064459efc4
Ange Kouakou <ange@swagwatch.app>
2026-08-20 03:48:46 +0000

    Deploy bounded Uriel model calls

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-fef3bd0";
+      url = "path:/mnt/data/vendor/uriel-90d3340";
       flake = true;
     };
   };
