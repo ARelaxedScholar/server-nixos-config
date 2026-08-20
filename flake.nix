4b1a9f867257f7478e255f5e62fae833ee83ddad
Ange Kouakou <ange@swagwatch.app>
2026-08-20 19:47:12 +0000

    deploy resilient tool-call normalization

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-5fa5799";
+      url = "path:/mnt/data/vendor/uriel-d60937d";
       flake = true;
     };
   };
