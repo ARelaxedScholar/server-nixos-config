c132c86283537c2cb79be9f5b1c6bd729c4ee7f7
Ange Kouakou <ange@swagwatch.app>
2026-08-20 15:20:11 +0000

    Deploy Uriel DeepSeek DSML adapter

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-5cb2b8f";
+      url = "path:/mnt/data/vendor/uriel-1b62128";
       flake = true;
     };
   };
