48f3640da0fee66a909908a2fdcd6b5a7e3dd5e3
Ange Kouakou <ange@swagwatch.app>
2026-08-20 15:10:11 +0000

    Deploy grounded Uriel commitment retries

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-51d2077";
+      url = "path:/mnt/data/vendor/uriel-5cb2b8f";
       flake = true;
     };
   };
