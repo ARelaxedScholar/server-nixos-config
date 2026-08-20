09d681fb1b1680a79ce1fc8968b68d2bcf94cb56
Ange Kouakou <ange@swagwatch.app>
2026-08-20 15:30:59 +0000

    Deploy alternate Uriel DSML parser

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-1b62128";
+      url = "path:/mnt/data/vendor/uriel-fb08a6c";
       flake = true;
     };
   };
