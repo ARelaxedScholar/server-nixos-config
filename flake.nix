a7d08ace016dd54b0218e1234dfdda5cd06627ec
Ange Kouakou <ange@swagwatch.app>
2026-08-20 20:17:00 +0000

    deploy self-repair authority guard

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-899pending";
+      url = "path:/mnt/data/vendor/uriel-8834final";
       flake = true;
     };
   };
