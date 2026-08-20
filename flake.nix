042cad3d5fba028e401e37ff9b6f100334b81db7
Ange Kouakou <ange@swagwatch.app>
2026-08-20 03:26:25 +0000

    Deploy Uriel single-flight commitments

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
     # GitLab SSH credentials. Directory suffixes identify the mirrored commits.
     watchtower.url = "path:/mnt/data/vendor/watchtower-464fb957";
     uriel = {
-      url = "path:/mnt/data/vendor/uriel-288303b";
+      url = "path:/mnt/data/vendor/uriel-fef3bd0";
       flake = true;
     };
   };
